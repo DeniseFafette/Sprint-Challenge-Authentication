6d3c17ff1a283eac5a1719eb33c0c0ca7c291b6b
DeniseFafette <denisefafette@gmail.com>
2020-01-31 09:44:12 -0800

    updated auth-router jwt registration and login

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,36 @@
 module.exports = {
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: { filename: './database/auth.db3' },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
       tableName: 'dbmigrations',
     },
+    pool: {
+      afterCreate:(conn, done) => {
+          conn.run('PRAGMA foreign_keys = ON', done);
+      }
+  },
+    seeds: { directory: './database/seeds' },
+  },
+
+  testing: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: { filename: './data/test.db3' },
+    useNullAsDefault: true,
+    migrations: {
+      directory: './database/migrations',
+      tableName: 'dbmigrations',
+    },
+    pool: {
+      afterCreate:(conn, done) => {
+          conn.run('PRAGMA foreign_keys = ON', done);
+      }
+  },
     seeds: { directory: './database/seeds' },
   },
 };
+
